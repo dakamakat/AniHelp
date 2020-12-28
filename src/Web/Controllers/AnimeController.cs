800de5afb66373c67e3f8521e5dda3594cddb5ba
Dakamakat <gazon-ozon@yandex.ru>
2020-12-28 23:09:51 +0300

    add project references

--- src/Web/Controllers/AnimeController.cs
+++ src/Web/Controllers/AnimeController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Controllers
 {
@@ -11,5 +6,6 @@ namespace Web.Controllers
     [Route("[controller]")]
     public class AnimeController : ControllerBase
     {
+
     }
 }
