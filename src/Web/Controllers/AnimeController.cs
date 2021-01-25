f619fa8eea5752da0ba3e0d23b566fc4b77fda2c
Dakamakat <gazon-ozon@yandex.ru>
2021-01-25 20:18:22 +0300

    fix: add new methods to controller and add settings to json serialization

--- src/Web/Controllers/AnimeController.cs
+++ src/Web/Controllers/AnimeController.cs
@@ -1,10 +1,6 @@
-﻿using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Application.AnimeLists.Queries;
 using Application.Common.Models;
-using Infrastructure.Services.KitsuApiService.Contracts;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Controllers
@@ -12,9 +8,16 @@ namespace Web.Controllers
     public class AnimeController : ApiControllerBase
     {
         [HttpGet]
-        public async Task<ActionResult<AnimeList>> Get()
+        public async Task<ActionResult<AnimeList>> GetAnimeList()
         {
             return Ok(await Mediator.Send(new GetAnimeListQuery()));
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AnimeList>> GetAnimeById(int id)
+        {
+            return Ok(await Mediator.Send(new GetAnimeByIdQuery { Id = id }));
+        }
+
     }
 }
--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using Application;
+using Flurl.Http;
+using Flurl.Http.Configuration;
 using Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
 using Serilog;
 
 namespace Web
@@ -34,9 +37,18 @@ namespace Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseCors(options => options.WithOrigins("http://localhost:3001")
+            app.UseCors(options => options.WithOrigins("http://localhost:9999", "http://localhost:10000")
                .AllowAnyHeader()
                .AllowAnyMethod());
+
+            FlurlHttp.Configure(settings => {
+                var jsonSettings = new JsonSerializerSettings
+                {
+                    NullValueHandling = NullValueHandling.Ignore,
+                    ObjectCreationHandling = ObjectCreationHandling.Replace
+                };
+                settings.JsonSerializer = new NewtonsoftJsonSerializer(jsonSettings);
+            });
 
             if (env.IsDevelopment())
             {
